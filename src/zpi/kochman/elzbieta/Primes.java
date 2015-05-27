ef075309e2d76391be42f5fd24e1d9333fcb583b
Elżbieta Kochman <e.w.kochman@gmail.com>
2015-05-27 21:55:58 +0200

    Utworzenie klasy wykonawczej i klasy testującej

---
+++ src/zpi/kochman/elzbieta/Primes.java
@@ -0,0 +1,5 @@
+package zpi.kochman.elzbieta;
+
+public class Primes {
+
+}
---
+++ src/zpi/kochman/elzbieta/test/PrimeTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package zpi.kochman.elzbieta.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Elżbieta Kochman
+ *
+ */
+public class PrimeTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
